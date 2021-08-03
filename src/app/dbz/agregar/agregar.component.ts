f3f32a627bedac3791d2abf3da22d3121165a0f2
Mauricio Gallego <ingmauricio05@hotmail.com>
2021-08-03 09:05:47 -0500

    Commit sección 5

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,34 @@
+import { Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html'
+})
+export class AgregarComponent {
+  
+  @Input() nuevo: Personaje = {
+    nombre: "",
+    poder: 0
+  }
+  
+ // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+  constructor(private dbzService: DbzService){
+  }
+
+  agregar(){
+    if( this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+
+    console.log(this.nuevo);
+    //this.onNuevoPersonaje.emit( this.nuevo);
+    this.dbzService.agregarPersonaje(this.nuevo);
+    this.nuevo = {
+      nombre: "",
+      poder: 0
+    }
+  }
+}
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,19 @@
+import { Component} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo:Personaje ={
+    nombre: "Maestro roshi",
+    poder: 10000
+  }
+
+  constructor( ){
+   
+  }
+}
